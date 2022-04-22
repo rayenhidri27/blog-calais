aa709c596af4d4eb3d2e5c48f595da6786f6ba87
Rayen HIDRI <rayen.hidri@gmail.com>
2022-04-22 14:29:37 +0200

    création des templates : accueil et article

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -36,7 +36,7 @@ class HomeController extends AbstractController
         if (!$article) {
             $this->redirectToRoute("home");
         }
-        return $this->render('show/index.html.twig', [
+        return $this->render('home/show.html.twig', [
             "article" => $article
         ]);
     }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -229,4 +229,9 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
         return $this;
     }
+
+    public function __toString()
+    {
+        return $this->firstName . ' ' . $this->lastName;
+    }
 }
