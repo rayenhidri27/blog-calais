03cd6e348ef9917a35f2413b9fdf38bef0bcfe05
Rayen HIDRI <rayen.hidri@gmail.com>
2022-04-28 11:11:01 +0200

    pagination et translation

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,6 +6,8 @@ use App\Entity\Article;
 use App\Entity\Category;
 use App\Repository\ArticleRepository;
 use App\Repository\CategoryRepository;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -24,12 +26,19 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index(): Response
+    public function index(Request $request, PaginatorInterface $paginator): Response
     {
         $categories = $this->repoCategory->findAll();
         $articles = $this->repoArticle->findAll();
+
+        $articlesPag = $paginator->paginate(
+            $articles, // Requête contenant les données à paginer (ici nos articles)
+            $request->query->getInt('page', 1), // Numéro de la page en cours, passé dans l'URL, 1 si aucune page
+            6 // Nombre de résultats par page
+        );
+        
         return $this->render('home/index.html.twig', [
-            "articles" => $articles,
+            "articles" => $articlesPag,
             "categories" => $categories
         ]);
     }
@@ -51,15 +60,20 @@ class HomeController extends AbstractController
     /**
      * @Route("/showArticles/{id}", name="show_articles")
      */
-    public function showArticles(?Category $category): Response
+    public function showArticles(?Category $category,Request $request, PaginatorInterface $paginator): Response
     {
         if ($category) {
             $articles = $category->getArticles()->getValues();
+            $articlesPag = $paginator->paginate(
+                $articles, // Requête contenant les données à paginer (ici nos articles)
+                $request->query->getInt('page', 1), // Numéro de la page en cours, passé dans l'URL, 1 si aucune page
+                6 // Nombre de résultats par page
+            );
         } else {
             return $this->redirectToRoute("home");
         }
         return $this->render('home/index.html.twig', [
-            "articles" => $articles,
+            "articles" => $articlesPag,
             "categories" => $this->repoCategory->findAll()
         ]);
     }
