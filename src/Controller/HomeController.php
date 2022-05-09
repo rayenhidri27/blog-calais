67491f304c7fcb9e4230ffeb19393200201ebca5
Rayen HIDRI <rayen.hidri@gmail.com>
2022-05-09 16:35:32 +0200

    rectif pagination recherche

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,6 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Session\Session;
 
 class HomeController extends AbstractController
 {
@@ -85,9 +86,19 @@ class HomeController extends AbstractController
     {
         $date = \DateTime::createFromFormat("Y-m-d", date($request->request->get('date')));
         //dd($date);
-        $articles = $this->repoArticle->findByTitleLike($request->request->get('title'), $date);
+
+        $session = new Session();
+        if(!$session->get('articles')) {            
+            $session->start();            
+        }
+        
+        if($request->request->get('title')){
+            $articles = $this->repoArticle->findByTitleLike($request->request->get('title'), $date);
+            $session->set('articles', $articles);
+        }
+        
         $articlesPag = $paginator->paginate(
-            $articles, // Requête contenant les données à paginer (ici nos articles)
+            $session->get('articles'), // Requête contenant les données à paginer (ici nos articles)
             $request->query->getInt('page', 1), // Numéro de la page en cours, passé dans l'URL, 1 si aucune page
             6 // Nombre de résultats par page
         );
